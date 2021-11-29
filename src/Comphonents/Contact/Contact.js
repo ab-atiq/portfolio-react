c525fe965c16c2409deabe9cbcef8d72b135908a
ATIQUR RAHMAN <abulbasharatiqur@gmail.com>
2021-11-30 03:54:29 +0600

    contact-problem

--- src/Comphonents/Contact/Contact.js
+++ src/Comphonents/Contact/Contact.js
@@ -1,10 +1,36 @@
 import React from 'react';
-
+import emailjs from 'emailjs-com';
+import { Container } from 'react-bootstrap';
+import './Contact.css';
 const Contact = () => {
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('gmail', 'template_h4w3a81', e.target, 'user_k1hkxGgcIjzozr8nSk82l')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+            e.target.reset();
+    };
+
     return (
-        <div>
+        <Container>
             <h1>Contact me</h1>
-        </div>
+            <form id="contact-form" onSubmit={sendEmail}>
+                <input type="hidden" name="contact_number" />
+                <label>Name</label>
+                <input type="text" name="user_name" />
+                <label>Email</label>
+                <input type="email" name="user_email" />
+                <label>Message</label>
+                <textarea name="message"></textarea>
+                <br />
+                <input type="submit" value="Send" />
+            </form>
+        </Container>
     );
 };
 
